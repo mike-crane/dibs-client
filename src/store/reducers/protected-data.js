9c06f56735ca091b583d961161ccb86883408cf6
Patrick Ford <patrickford@msn.com>
2018-07-16 08:29:05 -0700

    Added mock data to protected-data reducer

--- src/store/reducers/protected-data.js
+++ src/store/reducers/protected-data.js
@@ -1,20 +1,99 @@
 import {
-  FETCH_PROTECTED_DATA_SUCCESS,
-  FETCH_PROTECTED_DATA_ERROR
+  FETCH_PROPERTY_DATA_SUCCESS,
+  FETCH_PROPERTY_DATA_ERROR
 } from '../../actions/protected-data';
 
 const initialState = {
-  data: '',
-  error: null
+  user: {
+    firstName: "John",
+    lastName: "Doe",
+    username: "beachBum"
+  },
+  properties: [
+    {
+      id: 3423141,
+      name: "Beach House",
+      address: {
+        street: "150 Ocean Blvd",
+        state: "NC",
+        city: "Wrightsville",
+        zipcode: 28480
+      },
+      type: "house",
+      owner: "beachBum"
+    },
+    {
+      id: 9231230,
+      name: "Downtown Apartment",
+      address: {
+        street: "210 Main Street",
+        state: "NC",
+        city: "Charlotte",
+        zipcode: 28105
+      },
+      type: "apartment",
+      owner: "globeTr0ttr"
+    },
+    {
+      id: 7897234,
+      name: "Mountain House",
+      address: {
+        street: "340 Eagles Nest Way",
+        state: "NC",
+        city: "Asheville",
+        zipcode: 28715
+      },
+      type: "house",
+      owner: "mommaD05"
+    }
+  ],
+  reservations: [
+    {
+      propertyID: 3423141,
+      username: "mommaD05",
+      start: "5-2-2018",
+      end: "5-6-2018"
+    },
+    {
+      propertyID: 9231230,
+      username: "beachBum",
+      start: "6-22-2018",
+      end: "6-31-2018"
+    },
+    {
+      propertyID: 7897234,
+      username: "globeTr0ttr",
+      start: "8-12-2018",
+      end: "8-16-2018"
+    },
+    {
+      propertyID: 7897234,
+      username: "beachBum",
+      start: "8-25-2018",
+      end: "8-28-2018"
+    },
+    {
+      propertyID: 3423141,
+      username: "globeTr0ttr",
+      start: "9-2-2018",
+      end: "9-10-2018"
+    },
+    {
+      propertyID: 9231230,
+      username: "mommaD05",
+      start: "10-8-2018",
+      end: "10-10-2018"
+    }
+  ]
 };
 
 export default function reducer(state = initialState, action) {
-  if (action.type === FETCH_PROTECTED_DATA_SUCCESS) {
+  if (action.type === FETCH_PROPERTY_DATA_SUCCESS) {
     return Object.assign({}, state, {
       data: action.data,
       error: null
     });
-  } else if (action.type === FETCH_PROTECTED_DATA_ERROR) {
+  } else if (action.type === FETCH_PROPERTY_DATA_ERROR) {
     return Object.assign({}, state, {
       error: action.error
     });
