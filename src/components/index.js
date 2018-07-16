39a7c7e0b5e667b577c0db6d92dd3a8eb1b08d48
mike crane <mike.c0321@gmail.com>
2018-07-15 23:31:49 -0400

    add login and registration

--- src/components/index.js
+++ src/components/index.js
@@ -1,21 +1,60 @@
 import React, { Component } from "react";
-import logo from "../logo.svg";
+import { connect } from 'react-redux';
+import { Route, withRouter } from 'react-router-dom';
+
+import Header from "./Header";
+import Landing from "./Landing";
+import Dashboard from './Dashboard';
+import RegistrationPage from './Registration-page';
+import { refreshAuthToken } from '../actions/auth';
 import "../stylesheets/index.css";
 
 class App extends Component {
+  componentDidUpdate(prevProps) {
+    if (!prevProps.loggedIn && this.props.loggedIn) {
+      // When we are logged in, refresh the auth token periodically
+      this.startPeriodicRefresh();
+    } else if (prevProps.loggedIn && !this.props.loggedIn) {
+      // Stop refreshing when we log out
+      this.stopPeriodicRefresh();
+    }
+  }
+
+  componentWillUnmount() {
+    this.stopPeriodicRefresh();
+  }
+
+  startPeriodicRefresh() {
+    this.refreshInterval = setInterval(
+      () => this.props.dispatch(refreshAuthToken()),
+      60 * 60 * 1000 // One hour
+    );
+  }
+
+  stopPeriodicRefresh() {
+    if (!this.refreshInterval) {
+      return;
+    }
+
+    clearInterval(this.refreshInterval);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+        <Header />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/register" component={RegistrationPage} />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  hasAuthToken: state.auth.authToken !== null,
+  loggedIn: state.auth.currentUser !== null
+});
+
+// Deal with update blocking - https://reacttraining.com/react-router/web/guides/dealing-with-update-blocking
+export default withRouter(connect(mapStateToProps)(App));
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './stylesheets/index.css';
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
 import App from './components';
+import store from "./store";
 import registerServiceWorker from './registerServiceWorker';
+import "./stylesheets/index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
